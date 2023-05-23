4a23c07e5adce175b6511b044486c2334ab628d0
Danilo Lima <danilolima@MBP-de-Danilo.lan>
2023-05-23 13:35:32 +0100

    Lessons until viewport settings

---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,5 @@
+it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+    cy.visit('./src/privacy.html')
+
+    cy.contains('Talking About Testing').should('be.be.visible')
+})
